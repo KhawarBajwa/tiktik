88c47de588a48a6d10425235465e390c655ee467
unknown <khawarasasian@google.com>
2023-03-30 10:56:51 +0500

    first commit

---
+++ red-leopard/schemas/index.js
@@ -0,0 +1,10 @@
+
+import post from './post';
+import user from './user';
+import comment from './comment';
+import postedBy from './postedBy';
+
+export const schemaTypes = [ post,
+    user,
+    comment,
+    postedBy,]
